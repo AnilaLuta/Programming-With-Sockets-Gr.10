bc9bb0450a7026b2c628da85e243d4234f38d9dc
Anila Luta <118193110+AnilaLuta@users.noreply.github.com>
2024-11-13 14:27:40 +0100

    Update server.cs

--- server.cs
+++ server.cs
@@ -1,1 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
+class UDPServer
+{
+    private static IPEndPoint clientWithFullAccess = null;
+
+    static void Main()
+    {
+        Task.Run(async () => await StartServerAsync());
+
+        Console.ReadLine();
+    }
+
+    private static bool VerifyClientAccess(IPEndPoint clientAddress, string authMessage)
+    {
+        if (authMessage.StartsWith("CONNECT:ADMIN"))
+        {
+            string[] adminCredentials = authMessage.Substring(13).Split(':');
+
+            if (adminCredentials.Length == 2)
+            {
+                string adminUsername = adminCredentials[0];
+                string adminPassword = adminCredentials[1];
+
+                if (adminUsername == "admin" && adminPassword == "admin123")
+                {
+                    return true; 
+                }
+            }
+        }
+
+        return false;
+    }
